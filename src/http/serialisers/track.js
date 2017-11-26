fe5498c80dd89ea3270b43565dcf2ad378e45d78
Louis Capitanchik <contact@louiscap.co>
2017-11-26 00:21:01 +0000

    Fix track image

--- src/http/serialisers/track.js
+++ src/http/serialisers/track.js
@@ -1,8 +1,13 @@
 module.exports = function serialiseTrack(track) {
 	console.log(track)
+	const image = track.album.images.length > 0 ? track.album.images[0] : { url: null }
+	console.log(image)
 	return Object.assign(
 		{},
 		track,
-		{ artist: track.artists[0] }
+		{
+			artist: track.artists[0],
+			image: image.url,
+		}
 	)
 }
